2024f0a0a0e49b1936985f76f78c641783a634f4
Aman Gupta <aman930530@gmail.com>
2021-09-13 18:29:37 +0530

    fix static path

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
 const app = express(); 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const Blogs = require('./database/blogShema');
 const bodyParser = require('body-parser');
 
@@ -44,7 +44,7 @@ app.post('/upload-blogs', async(req,res)=>
 })
 
 if(process.env.NODE_ENV == "production"){
-    app.use(express.static('react-blog/build'))
+    app.use(express.static('client/build'))
     const path = require('path')
     app.get("*",(req,res)=>
     {
@@ -52,6 +52,6 @@ if(process.env.NODE_ENV == "production"){
     })
 }
 
-app.listen(port, () =>{
-    console.log(`Server is running at port no ${port}`)
+app.listen(PORT, () =>{
+    console.log(`Server is running at port no ${PORT}`)
 })
